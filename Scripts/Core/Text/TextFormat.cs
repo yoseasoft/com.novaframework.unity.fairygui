6d75c4a35bac5c7f8566fa871a6d5bb8d6a5c766
hurley <yoseasoft@163.com>
2025-07-30 14:50:36 +0800

    部署 NovaFramework 框架所需的 FairyGUI 插件；

---
+++ Scripts/Core/Text/TextFormat.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+
+namespace FairyGUI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class TextFormat
+    {
+        public enum SpecialStyle
+        {
+            None,
+            Superscript,
+            Subscript
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int size;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string font;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color color;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int lineSpacing;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int letterSpacing;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool bold;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool underline;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool italic;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool strikethrough;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color32[] gradientColor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public AlignType align;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SpecialStyle specialStyle;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float outline;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color outlineColor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Vector2 shadowOffset;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Color shadowColor;
+
+        // 参考:https://ask.fairygui.com/?/article/6555
+        #region 连字保护（ContinuousWordProtect）
+
+        /// <summary>
+        /// 是否启用连字保护
+        /// </summary>
+        public bool cwpEnabled;
+
+        /// <summary>
+        /// 连字保护是否启用拉伸（类似 Office-Word 的效果）
+        /// </summary>
+        public bool cwpStretchEnabled;
+
+        /// <summary>
+        /// 连字保护最小字距
+        /// </summary>
+        public float cwpMinLetterSpacing;
+
+        #endregion
+
+        public TextFormat()
+        {
+            color = Color.black;
+            size = 12;
+            lineSpacing = 3;
+            outlineColor = shadowColor = Color.black;
+
+            cwpEnabled = UIConfig.cwpEnabled;
+            cwpStretchEnabled = UIConfig.cwpStretchEnabled;
+            cwpMinLetterSpacing = UIConfig.cwpMinLetterSpacing;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetColor(uint value)
+        {
+            uint rr = (value >> 16) & 0x0000ff;
+            uint gg = (value >> 8) & 0x0000ff;
+            uint bb = value & 0x0000ff;
+            float r = rr / 255.0f;
+            float g = gg / 255.0f;
+            float b = bb / 255.0f;
+            color = new Color(r, g, b, 1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="aFormat"></param>
+        /// <returns></returns>
+        public bool EqualStyle(TextFormat aFormat)
+        {
+            return size == aFormat.size && color == aFormat.color
+                && bold == aFormat.bold && underline == aFormat.underline
+                && italic == aFormat.italic
+                && strikethrough == aFormat.strikethrough
+                && gradientColor == aFormat.gradientColor
+                && align == aFormat.align
+                && specialStyle == aFormat.specialStyle;
+        }
+
+        /// <summary>
+        /// Only base NOT all formats will be copied
+        /// </summary>
+        /// <param name="source"></param>
+        public void CopyFrom(TextFormat source)
+        {
+            this.size = source.size;
+            this.font = source.font;
+            this.color = source.color;
+            this.lineSpacing = source.lineSpacing;
+            this.letterSpacing = source.letterSpacing;
+            this.bold = source.bold;
+            this.underline = source.underline;
+            this.italic = source.italic;
+            this.strikethrough = source.strikethrough;
+            if (source.gradientColor != null)
+            {
+                this.gradientColor = new Color32[4];
+                source.gradientColor.CopyTo(this.gradientColor, 0);
+            }
+            else
+                this.gradientColor = null;
+            this.align = source.align;
+            this.specialStyle = source.specialStyle;
+        }
+
+        public void FillVertexColors(Color32[] vertexColors)
+        {
+            if (gradientColor == null)
+                vertexColors[0] = vertexColors[1] = vertexColors[2] = vertexColors[3] = color;
+            else
+            {
+                vertexColors[0] = gradientColor[1];
+                vertexColors[1] = gradientColor[0];
+                vertexColors[2] = gradientColor[2];
+                vertexColors[3] = gradientColor[3];
+            }
+        }
+    }
+}
---
+++ Scripts/Core/Text/WXScreenKeyboard.cs
@@ -0,0 +1,144 @@
+﻿#if UNITY_WEBGL || WEIXINMINIGAME
+using UnityEngine;
+using WeChatWASM;
+
+namespace FairyGUI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WXScreenKeyboard : IKeyboard
+    {
+        UnityEngine.TouchScreenKeyboard _keyboard;
+        private bool _showKeyboard = false;
+        public bool done
+        {
+#if UNITY_2017_2_OR_NEWER
+            get
+            {
+                return _keyboard == null
+              || _keyboard.status == UnityEngine.TouchScreenKeyboard.Status.Done
+              || _keyboard.status == UnityEngine.TouchScreenKeyboard.Status.Canceled
+              || _keyboard.status == UnityEngine.TouchScreenKeyboard.Status.LostFocus;
+            }
+#else
+            get { return _keyboard == null || _keyboard.done || _keyboard.wasCanceled; }
+#endif
+        }
+
+        public bool supportsCaret
+        {
+            get { return false; }
+        }
+
+        public string GetInput()
+        {
+            if (_keyboard != null)
+            {
+                string s = _keyboard.text;
+
+                if (this.done)
+                    _keyboard = null;
+
+                return s;
+            }
+            else
+                return null;
+        }
+
+        public void Open(string text, bool autocorrection, bool multiline, bool secure, bool alert, string textPlaceholder, int keyboardType, bool hideInput)
+        {
+            if (_keyboard != null)
+                return;
+
+            ShowKeyboard();
+
+            UnityEngine.TouchScreenKeyboard.hideInput = hideInput;
+            _keyboard = UnityEngine.TouchScreenKeyboard.Open(text, (TouchScreenKeyboardType)keyboardType, autocorrection, multiline, secure, alert, textPlaceholder);
+            Debug.Log($"----- Open text = {text}");
+        }
+
+        public void Close()
+        {
+            if (_keyboard != null)
+            {
+                HideKeyboard();
+                
+                _keyboard.active = false;
+                _keyboard = null;
+            }
+        }
+        
+        #region WX API
+        private void ShowKeyboard()
+        {
+            if (!_showKeyboard)
+            {
+                ShowKeyboardOption callback = new ShowKeyboardOption()
+                {
+                    defaultValue = "xxx",
+                    maxLength = 20,
+                    confirmType = "go"
+                };
+                WX.ShowKeyboard(callback);
+
+                //绑定回调
+                WX.OnKeyboardConfirm(OnConfirm);
+                WX.OnKeyboardComplete(OnComplete);
+                WX.OnKeyboardInput(OnInput);
+            
+                Debug.Log($"----- 显示键盘成功 绑定回调");
+            }
+            else
+            {
+                Debug.Log($"----- 显示键盘失败 _showKeyboard = {_showKeyboard}");
+            }
+        }
+
+        private void HideKeyboard()
+        {
+            if (_showKeyboard)
+            {
+                WX.HideKeyboard(new HideKeyboardOption());
+                //删除掉相关事件监听
+                WX.OffKeyboardInput(OnInput);
+                WX.OffKeyboardConfirm(OnConfirm);
+                WX.OffKeyboardComplete(OnComplete);
+                _showKeyboard = false;
+                
+                Debug.Log($"----- 隐藏键盘成功");
+            }
+            else
+            {
+                Debug.Log($"----- 隐藏键盘失败");
+            }
+        }
+        
+        public void OnInput(OnKeyboardInputListenerResult v)
+        {
+            Debug.Log($"----- VX回调 OnInput text = {v.value}");
+            // if (input.isFocused)
+            // {
+            //     input.text = v.value;
+            //     Debug.Log($"----- OnInput Focused");
+            // }
+        }
+
+        public void OnConfirm(OnKeyboardInputListenerResult v)
+        {
+            // 输入法confirm回调
+            Debug.Log($"----- VX回调 OnConfirm {v.value}");
+            HideKeyboard();
+        }
+
+        public void OnComplete(OnKeyboardInputListenerResult v)
+        {
+            // 输入法complete回调
+            Debug.Log($"----- VX回调 OnComplete {v.value}");
+            HideKeyboard();
+        }
+        
+        #endregion
+    }
+}
+#endif
